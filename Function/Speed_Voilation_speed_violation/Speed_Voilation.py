cfff4a5fac0e350aeb17a3f6b19446d95de91e1f
rohitkumar0427 <61402931+rohitkumar0427@users.noreply.github.com>
2020-03-06 15:30:25 +0530

    Update Speed_Voilation.py

--- Function/Speed_Voilation_speed_violation/Speed_Voilation.py
+++ Function/Speed_Voilation_speed_violation/Speed_Voilation.py
@@ -1,9 +1,11 @@
-import os
+"""
+Function checks for speed voilation at the certain location.
+"""
+
 import pandas as pd
 
-def speed_violation(speed,GpsTime, Latitude,Longitude,Threshold_Speed):
+def speed_violation(speed, Latitude,Longitude,Threshold_Speed):
 	SPEED_VIOLATION = []
-	GPS_TIME = []
 	
 	for i in speed.index:
 		if speed[i]== '-' :
@@ -12,8 +14,3 @@ def speed_violation(speed,GpsTime, Latitude,Longitude,Threshold_Speed):
 			SPEED_VIOLATION.append([Latitude[i],Longitude[i],GpsTime[i],speed[i],i])
 	VIOLATION = pd.DataFrame(data=SPEED_VIOLATION, columns=['Latitude', 'Longitude','GPS_Time','Speed','Index'])
 	return VIOLATION
-
-
-
-
-
