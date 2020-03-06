8684c1d1871e1c94eb979761dd09a9b79a0b85ed
rohitkumar0427 <61402931+rohitkumar0427@users.noreply.github.com>
2020-03-06 14:04:58 +0530

    Update DIAFuelMixture.py

--- Function/DIAFuelMixture_FuelMixture/DIAFuelMixture.py
+++ Function/DIAFuelMixture_FuelMixture/DIAFuelMixture.py
@@ -1,26 +1,28 @@
+""" This function checks the ratio of air-fuel mixture and classify them as the Lean or Rich mixture.
+1. When the oxygen value in the sensor is less than 0.1, it is classified as the Lean mixture.
+2. When the O2 value is between 0.9 to 1.0, it is classified as the Rich mixture.
+"""
+
 import pandas as pd
 
-def Fuel_Mixture(O2_Volts):
-	lean = []
-	rich = []
-	nrml = []
-	LEAN = pd.DataFrame(data=lean, columns=['O2_Volts','Index'])				#"Engine is in safe state")
-	RICH = pd.DataFrame(data=rich, columns=['O2_Volts','Index'])				#"Engine is in safe state")
-	NRML = pd.DataFrame(data=nrml, columns=['O2_Volts','Index'])				#"Engine is in safe state")
+def FuelMixture(O2_Volts):
+	Lean = []
+	Rich = []
+	Nrml = []
 
 	for i in range(0, len(O2_Volts)):
 		if O2_Volts[i]== '-' :
 			O2_Volts[i]= '0';
 		O2_Volts[i] = float(O2_Volts[i])		
 		if O2_Volts[i] >= 0.0 and O2_Volts[i] <= 0.1:
-			lean.append([O2_Volts[i], i])
-			LEAN = pd.DataFrame(data=lean, columns=['O2_Volts','Index'])
+			Lean.append([O2_Volts[i], i])
 		elif O2_Volts[i] >= 0.9 and O2_Volts[i] <= 1.0:
-			rich.append([O2_Volts[i], i])
-			RICH = pd.DataFrame(data=rich, columns=['O2_Volts','Index'])
+			Rich.append([O2_Volts[i], i])
 		else:
-			nrml.append([O2_Volts[i], i])
-			NRML = pd.DataFrame(data=nrml, columns=['O2_Volts','Index'])
+			Nrml.append([O2_Volts[i], i])
+	LEAN = pd.DataFrame(data=Lean, columns=['O2_Volts','Index'])
+	RICH = pd.DataFrame(data=Rich, columns=['O2_Volts','Index'])
+	NRML = pd.DataFrame(data=Nrml, columns=['O2_Volts','Index'])
 	
 	return LEAN, RICH, NRML
 		
