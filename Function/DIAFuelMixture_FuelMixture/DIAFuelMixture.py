8daed22fd9ec74f22055d5feeb21f478ee788b28
rohitkumar0427 <61402931+rohitkumar0427@users.noreply.github.com>
2020-03-06 16:17:19 +0530

    Update DIAFuelMixture.py

--- Function/DIAFuelMixture_FuelMixture/DIAFuelMixture.py
+++ Function/DIAFuelMixture_FuelMixture/DIAFuelMixture.py
@@ -1,4 +1,5 @@
-"""Function checks the ratio of air-fuel mixture and classify them as the Lean or Rich mixture.
+"""
+Function checks the ratio of air-fuel mixture and classify them as the Lean or Rich mixture.
 1. When the oxygen value in the sensor is less than 0.1, it is classified as the Lean mixture.
 2. When the O2 value is between 0.9 to 1.0, it is classified as the Rich mixture.
 """
