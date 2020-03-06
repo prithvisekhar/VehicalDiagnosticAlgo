8d2638b8c6c635e1502114b3e860ebd8d619e135
rohitkumar0427 <61402931+rohitkumar0427@users.noreply.github.com>
2020-03-06 18:08:41 +0530

    Update TestFuelMixture.py

--- Function/DIAFuelMixture_FuelMixture/TestFuelMixture.py
+++ Function/DIAFuelMixture_FuelMixture/TestFuelMixture.py
@@ -5,25 +5,25 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 class TestFuel_Mixture(unittest.TestCase):
-	def testFuelMixture(self):
+	def TestFuelMixture(self):
 		try:
 			df_File = pd.read_csv("List_of_Data_Set.csv")
 			if not(os.path.isdir("Result")):
 				os.mkdir("Result")
 			for i in df_File.index:
 				df = pd.read_excel(str(df_File["Input_File_Name"][i]))
-				temp_File1=df_File["Input_File_Name"][i].split('/')
-				temp_File3=temp_File1[-1].split('.')
-				temp_File4=str(temp_File3[0])
-				path=os.path.join("Result/" , temp_File4)
+				TempFile1=df_File["Input_File_Name"][i].split('/')
+				TempFile3=TempFile1[-1].split('.')
+				TempFile4=str(TempFile3[0])
+				path=os.path.join("Result/" , TempFile4)
 				Lean, Rich, Nrml =DIAFuelMixture.Fuel_Mixture(df['O2 Volts Bank 1 sensor 2(V)'])
-				temp_O2Sensor=df['O2 Volts Bank 1 sensor 2(V)'].replace(to_replace='-',value=0)
+				TempO2Sensor=df['O2 Volts Bank 1 sensor 2(V)'].replace(to_replace='-',value=0)
 				plt.figure()
-				plt.plot(np.repeat(0.1,len(temp_O2Sensor)),label='0.1')
-				plt.plot(np.repeat(0.0,len(temp_O2Sensor)),label='0.0')
-				plt.plot(np.repeat(0.9,len(temp_O2Sensor)),label='0.9')
-				plt.plot(np.repeat(1.0,len(temp_O2Sensor)),label='1.0')
-				plt.plot(temp_O2Sensor,marker='o',label='O2 Bank1')
+				plt.plot(np.repeat(0.1,len(TempO2Sensor)),label='0.1')
+				plt.plot(np.repeat(0.0,len(TempO2Sensor)),label='0.0')
+				plt.plot(np.repeat(0.9,len(TempO2Sensor)),label='0.9')
+				plt.plot(np.repeat(1.0,len(TempO2Sensor)),label='1.0')
+				plt.plot(TempO2Sensor,marker='o',label='O2 Bank1')
 				plt.plot(Lean['Index'],Lean['O2_Volts'],'r.',label='Lean')
 				plt.plot(Rich['Index'],Rich['O2_Volts'],'y.',label='Rich')
 				plt.plot(Nrml['Index'],Nrml['O2_Volts'],'g.',label='Normal')
