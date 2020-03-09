58dc2b858ece6e49b946a881a1060ae49fe7a203
rohitkumar0427 <61402931+rohitkumar0427@users.noreply.github.com>
2020-03-09 05:43:54 +0530

    Update and rename Test_Speed_Voilation.py to TestSpeedVoilation.py

--- Function/Speed_Voilation_speed_violation/TestSpeedVoilation.py
+++ Function/Speed_Voilation_speed_violation/TestSpeedVoilation.py
@@ -1,22 +1,22 @@
 import unittest
-import Speed_Voilation
+import DIASpeedVoilation
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
 class TestSpeed_Voilation(unittest.TestCase):
-	def testspeed_violation(self):
+	def TestSpeedViolation(self):
 		try:
 			df_File = pd.read_csv("List_of_Data_Set.csv")
 			if not(os.path.isdir("Result")):
 				os.mkdir("Result")
 			for i in df_File.index:
 				df = pd.read_excel(str(df_File["Input_File_Name"][i]))
-				Speed_violate=Speed_Voilation.speed_violation(df["Speed (GPS)(km/h)"],df[' Latitude'],df[' Longitude'],20)
+				SpeedViolate=DIASpeedVoilation.SpeedViolation(df["Speed (GPS)(km/h)"],df[' Latitude'],df[' Longitude'],20)
 				t=df["Speed (GPS)(km/h)"]
 				t2=t.replace(to_replace='-',value=0)
 				plt.figure()
 				plt.plot(t2,marker='o')
-				plt.plot(Speed_violate['Index'],Speed_violate['Speed'], 'ro')
+				plt.plot(SpeedViolate['Index'],SpeedViolate['Speed'], 'ro')
 				t1=df_File["Input_File_Name"][i].split('/')
 				t3=t1[-1].split('.')
 				plt.savefig('Result/'+str(t3[0])+'.png')
