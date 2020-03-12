936b39100796627e139c49215474d6dff5b02041
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-03-12 07:18:22 +0530

    Speed Voilation

---
+++ Function/DIASpeedVoilation_SpeedVoilation/DIASpeedVoilation.py
@@ -0,0 +1,13 @@
+"""Function checks for Speed voilation at the certain location."""
+
+import pandas as pd
+
+def SpeedVoilation(Speed, Latitude,Longitude,ThresholdSpeed):
+	TempSpeedVoilation = []
+	for i in Speed.index:
+		if Speed[i]== '-' :
+			Speed[i]=0
+		if (float(Speed[i])>ThresholdSpeed):
+			TempSpeedVoilation.append([Latitude[i],Longitude[i],Speed[i],i])
+	SpeedVoilation = pd.DataFrame(data=TempSpeedVoilation, columns=['Latitude', 'Longitude','Speed','Index'])
+	return SpeedVoilation
