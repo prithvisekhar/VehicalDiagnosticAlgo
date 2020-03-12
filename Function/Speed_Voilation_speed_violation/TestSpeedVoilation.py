af9d0577bddaddd8b4cc1ea9d83416a41194ee89
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-03-12 06:50:44 +0530

    Changed

--- Function/Speed_Voilation_speed_violation/TestSpeedVoilation.py
+++ Function/Speed_Voilation_speed_violation/TestSpeedVoilation.py
@@ -4,22 +4,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 class TestSpeed_Voilation(unittest.TestCase):
-	def TestSpeedViolation(self):
+	def testSpeedViolation(self):
 		try:
 			df_File = pd.read_csv("List_of_Data_Set.csv")
 			if not(os.path.isdir("Result")):
 				os.mkdir("Result")
 			for i in df_File.index:
-				df = pd.read_excel(str(df_File["Input_File_Name"][i]))
-				SpeedViolate=DIASpeedVoilation.SpeedViolation(df["Speed (GPS)(km/h)"],df[' Latitude'],df[' Longitude'],20)
-				t=df["Speed (GPS)(km/h)"]
-				t2=t.replace(to_replace='-',value=0)
+				df = pd.read_csv(str(df_File["Input_File_Name"][i]))
+				SpeedViolate=DIASpeedVoilation.SpeedVoilation(df["Speed (GPS)(km/h)"],df[' Latitude'],df[' Longitude'],20)
+				TempSpeed=df["Speed (GPS)(km/h)"].replace(to_replace='-',value=0)
 				plt.figure()
-				plt.plot(t2,marker='o')
+				plt.plot(TempSpeed,marker='o')
 				plt.plot(SpeedViolate['Index'],SpeedViolate['Speed'], 'ro')
-				t1=df_File["Input_File_Name"][i].split('/')
-				t3=t1[-1].split('.')
-				plt.savefig('Result/'+str(t3[0])+'.png')
+				TempFile1=df_File["Input_File_Name"][i].split('/')
+				TempFile3=TempFile1[-1].split('.')
+				plt.savefig('Result/'+str(TempFile3[0])+'.png')
 
 		except AssertionError as e:
 			f = open("Difference_Report_Speed_Voilation", "a")
