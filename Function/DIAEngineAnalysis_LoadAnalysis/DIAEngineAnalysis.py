9397cae7b74d9a81699c3ea928c76965af1bc1da
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-03-16 12:39:31 +0530

    Update DIAEngineAnalysis.py

--- Function/DIAEngineAnalysis_LoadAnalysis/DIAEngineAnalysis.py
+++ Function/DIAEngineAnalysis_LoadAnalysis/DIAEngineAnalysis.py
@@ -53,7 +53,7 @@ def LoadAnalysis(EngineLoad,EngineRPM,VehicleSpeed, TripTime):
 			TempEngineRPMMore.append([EngineRPM[i], i])
 
 		if EngineLoad[i] > LoadThreshold and  EngineRPM[i] > RPMThreshold:
-			temp_CounterOverload = temp_CounterOverload + 1 # Checking whether engine load and engine rpm are less than threshold
+			TempCounterOverload = TempCounterOverload + 1 # Checking whether engine load and engine rpm are less than threshold
 		CounterOverload.append(temp_CounterOverload)
 
 		VehicleSpeed[i] = float(VehicleSpeed[i])
