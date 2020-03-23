9fc99ad996d364627d6376c300e33bfc7cba832e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-23 05:20:29 +0000

    Deploy prithvisekhar/VehicalDiagnosticAlgo to github.com/prithvisekhar/VehicalDiagnosticAlgo.git:gh-pages

--- Algorithm/Integrated_Features.py
+++ Algorithm/Integrated_Features.py
@@ -3,6 +3,7 @@ from importlib.machinery import SourceFileLoader
 
 DIAEngineAnalysis = SourceFileLoader("LoadAnalysis", "../Function/DIAEngineAnalysis_Coolant/DIAEngineAnalysis.py").load_module()
 DIAFuelMixture = SourceFileLoader("FuelMixture", "../Function/DIAFuelMixture_FuelMixture/DIAFuelMixture.py").load_module()
+DIASpeedVoilation = SourceFileLoader("SpeedVoilation", "../Function/DIASpeedVoilation_SpeedVoilation/DIASpeedVoilation.py").load_module()
 
 df = pd.read_csv("../Datasets/Dataset-1.csv")
 
@@ -12,4 +13,7 @@ CoolantTemperatureF_np,HighestTemperatureC,NormalTemperatureC,LowestTemperatureC
  =DIAEngineAnalysis.Coolant(df['Engine Coolant Temperature(°C)'].replace(to_replace="-", value="0"),df['Engine Load(%)'].replace(to_replace="-", value="0"),df['Trip Time(Since journey start)(s)'].replace(to_replace="-", value="0"))
 
 Lean, Rich, Nrml =DIAFuelMixture.FuelMixture(df['O2 Volts Bank 1 sensor 2(V)'])
+
+SpeedViolate=DIASpeedVoilation.SpeedVoilation(df["Speed (GPS)(km/h)"],df[' Latitude'],df[' Longitude'],20)
+
 
