f938ddde8a53a9498cdfde0c663a07695182c65b
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-03-23 07:52:58 +0530

    Add files via upload

---
+++ Algorithm/Integrated_Features.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from importlib.machinery import SourceFileLoader
+
+DIAEngineAnalysis = SourceFileLoader("LoadAnalysis", "../Function/DIAEngineAnalysis_Coolant/DIAEngineAnalysis.py").load_module()
+DIAFuelMixture = SourceFileLoader("FuelMixture", "../Function/DIAFuelMixture_FuelMixture/DIAFuelMixture.py").load_module()
+
+df = pd.read_csv("../Datasets/Dataset-1.csv")
+
+EngineLoadLess, EngineLoadMore, EngineRPMLess, EngineRPMMore, VehicleSpeedLess, VehicleSpeedMore, ExpectedSpeed,LoadThreshold,RPMThreshold,CounterOverload = DIAEngineAnalysis.LoadAnalysis(df["Engine Load(%)"], df["Engine RPM(rpm)"],df['Speed (GPS)(km/h)'],df['Trip Time(Since journey start)(s)'])
+
+CoolantTemperatureF_np,HighestTemperatureC,NormalTemperatureC,LowestTemperatureC,SafeState1,SafeState2,SafeState3,State0,IndexTripTimeThreshold,EngineLoadThreshold \
+ =DIAEngineAnalysis.Coolant(df['Engine Coolant Temperature(°C)'].replace(to_replace="-", value="0"),df['Engine Load(%)'].replace(to_replace="-", value="0"),df['Trip Time(Since journey start)(s)'].replace(to_replace="-", value="0"))
+
+Lean, Rich, Nrml =DIAFuelMixture.FuelMixture(df['O2 Volts Bank 1 sensor 2(V)'])
+
