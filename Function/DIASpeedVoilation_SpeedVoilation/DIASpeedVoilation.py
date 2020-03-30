3af49fa5ea3d798e6aa81734021c26b02434aeb7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-30 15:23:25 +0000

    Deploy prithvisekhar/VehicalDiagnosticAlgo to github.com/prithvisekhar/VehicalDiagnosticAlgo.git:gh-pages

--- Function/DIASpeedVoilation_SpeedVoilation/DIASpeedVoilation.py
+++ Function/DIASpeedVoilation_SpeedVoilation/DIASpeedVoilation.py
@@ -2,11 +2,9 @@ import pandas as pd
 
 
 def SpeedVoilation(Speed, Latitude, Longitude, ThresholdSpeed):
-    TempSpeedVoilation = []
-    for i in Speed.index:
-        if Speed[i] == '-':
-            Speed[i] = 0
-        if(float(Speed[i]) > ThresholdSpeed):
-            TempSpeedVoilation.append([Latitude[i], Longitude[i], Speed[i], i])
-    SpeedVoilation = pd.DataFrame(data=TempSpeedVoilation, columns=['Latitude', 'Longitude', 'Speed', 'Index'])
-    return SpeedVoilation
+	TempSpeedVoilation = []
+	for i in Speed.index:
+		if (float(Speed[i]) > ThresholdSpeed):
+			TempSpeedVoilation.append([Latitude[i], Longitude[i], Speed[i], i])
+	SpeedVoilation = pd.DataFrame(data=TempSpeedVoilation, columns=['Latitude', 'Longitude', 'Speed', 'Index'])
+	return SpeedVoilation
