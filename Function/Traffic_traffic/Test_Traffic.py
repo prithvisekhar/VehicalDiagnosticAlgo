40c8510e9a46f9474cc7a8d36de5d86ab5435bbe
sonalik23 <62057255+sonalik23@users.noreply.github.com>
2020-03-30 20:56:30 +0530

    Update Test_Traffic.py

--- Function/Traffic_traffic/Test_Traffic.py
+++ Function/Traffic_traffic/Test_Traffic.py
@@ -11,13 +11,16 @@ class TestTraffic(unittest.TestCase):
             df_File = pd.read_csv("List_of_Data_Set.csv")
             os.system('mkdir Result')
             for i in df_File.index:
-                df = pd.read_excel(str(df_File["Input_File_Name"][i]))
+                df = pd.read_csv(df_File["Input_File_Name"][i])
                 t1 = df_File["Input_File_Name"][i].split('/')
                 t3 = t1[-1].split('.')
                 t4 = str(t3[0])
                 path = os.path.join("Result/", t4)
                 os.mkdir(path)
-                HC, MC, LC = Traffic.traffic(df["Speed (GPS)(km/h)"], df[' Latitude'], df[' Longitude'])
+                # os.chdir(path)
+                df["Speed (GPS)(km/h)"] = df["Speed (GPS)(km/h)"].astype("float")
+                HC, MC, LC = Traffic.traffic(df["Speed (GPS)(km/h)"].replace(to_replace='-', value=0), df[' Latitude'],
+                                             df[' Longitude'])
                 t = df["Speed (GPS)(km/h)"]
                 t2 = t.replace(to_replace='-', value=0)
                 plt.figure(1)
@@ -25,27 +28,35 @@ class TestTraffic(unittest.TestCase):
                 plt.plot(MC.loc[:, 'Longitude'], MC.loc[:, 'Latitude'], 'b.')
                 plt.plot(LC.loc[:, 'Longitude'], LC.loc[:, 'Latitude'], 'g.')
                 plt.title("Congestion Location")
-                plt.savefig(path+"/Congestion Location"+'.png')
+                plt.savefig(path + "/Congestion Location" + '.png')
+
                 plt.figure(2)
                 plt.plot(HC.loc[:, 'Speed'], HC.loc[:, 'Index'], 'r.')
                 plt.plot(MC.loc[:, 'Speed'], MC.loc[:, 'Index'], 'b.')
                 plt.plot(LC.loc[:, 'Speed'], LC.loc[:, 'Index'], 'g.')
                 plt.title("Speed vs Index")
-                plt.savefig(path+"/Speed vs Index"+'.png')
+                plt.savefig(path + "/Speed vs Index" + '.png')
+
                 plt.figure(3)
                 plt.plot(HC.loc[:, 'Latitude'], HC.loc[:, 'Index'], 'r.')
                 plt.plot(MC.loc[:, 'Latitude'], MC.loc[:, 'Index'], 'b.')
                 plt.plot(LC.loc[:, 'Latitude'], LC.loc[:, 'Index'], 'g.')
                 plt.title("Latitude vs Index")
-                plt.savefig(path+"/Latitude vs Index"+'.png')
+                plt.savefig(path + "/Latitude vs Index" + '.png')
+
                 plt.figure(4)
                 plt.plot(HC.loc[:, 'Longitude'], HC.loc[:, 'Index'], 'r.')
                 plt.plot(MC.loc[:, 'Longitude'], MC.loc[:, 'Index'], 'b.')
                 plt.plot(LC.loc[:, 'Longitude'], LC.loc[:, 'Index'], 'g.')
                 plt.title("Longitude vs Index")
-                plt.savefig(path+"/Longitude vs Index"+'.png')
+                plt.savefig(path + "/Longitude vs Index" + '.png')
+
+        # os.system('cd..')
+
         except AssertionError as e:
             f = open("Difference_Report_Speed_Voilation", "a")
-            f.write("TestCase_no_0:\n\t"+str(e)+" \n")
+            f.write("TestCase_no_0:\n\t" + str(e) + " \n")
+
+
 if __name__ == '__main__':
     unittest.main()
