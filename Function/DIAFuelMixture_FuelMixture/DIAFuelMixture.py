8a1cdb053133010ad73bbc07e1b10fbd24e0bca6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-30 15:09:38 +0000

    Deploy prithvisekhar/VehicalDiagnosticAlgo to github.com/prithvisekhar/VehicalDiagnosticAlgo.git:gh-pages

--- Function/DIAFuelMixture_FuelMixture/DIAFuelMixture.py
+++ Function/DIAFuelMixture_FuelMixture/DIAFuelMixture.py
@@ -1,19 +1,16 @@
 import pandas as pd
 
 
-def FuelMixture(O2_Volts):
+def Fuel_Mixture(O2_Volts):
     TempLean = []
     TempRich = []
     TempNrml = []
 
     for i in O2_Volts.index:
-        if O2_Volts[i] == '-':
-            O2_Volts[i] = '0'
         O2_Volts[i] = float(O2_Volts[i])
-        O2_Volts[i] = float(O2_Volts[i])
-        if O2_Volts[i] >= 0.0 and O2_Volts[i] <= 0.1:
+        if O2_Volts[i] <= 0.1:
             TempLean.append([O2_Volts[i], i])
-        elif O2_Volts[i] >= 0.9 and O2_Volts[i] <= 1.0:
+        elif O2_Volts[i] >= 0.9:
             TempRich.append([O2_Volts[i], i])
         else:
             TempNrml.append([O2_Volts[i], i])
