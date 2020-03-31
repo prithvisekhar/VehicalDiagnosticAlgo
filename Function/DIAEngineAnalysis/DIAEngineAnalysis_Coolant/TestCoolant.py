028707dc52cda28bfeef139135a832d2c736fab3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-31 10:27:26 +0000

    Deploy prithvisekhar/VehicalDiagnosticAlgo to github.com/prithvisekhar/VehicalDiagnosticAlgo.git:gh-pages

--- Function/DIAEngineAnalysis/DIAEngineAnalysis_Coolant/TestCoolant.py
+++ Function/DIAEngineAnalysis/DIAEngineAnalysis_Coolant/TestCoolant.py
@@ -22,30 +22,37 @@ class TestCoolant_Temperature(unittest.TestCase):
                 TempFile4 = str(TempFile3[0])
                 path = os.path.join("Result/", TempFile4)
 
-                CoolantTemperatureF_np, HighestTemperatureC, NormalTemperatureC, LowestTemperatureC, SafeState1, SafeState2, SafeState3, State0, IndexTripTimeThreshold, EngineLoadThreshold = DIAEngineAnalysis.Coolant(
-                    df['Engine Coolant Temperature(°C)'].replace(to_replace="-", value="0"),
+                CoolantTemperatureF_np, HighestTemperatureC, \
+                NormalTemperatureC, LowestTemperatureC, SafeState1, \
+                SafeState2, SafeState3, State0, IndexTripTimeThreshold, \
+                EngineLoadThreshold = DIAEngineAnalysis.Coolant(
+                    df['Engine Coolant Temperature(°C)'].replace("-", "0"),
                     df['Engine Load(%)'].replace(to_replace="-", value="0"),
-                    df['Trip Time(Since journey start)(s)'].replace(to_replace="-", value="0"))
+                    df['Trip Time(Since journey start)(s)'].replace("-", "0"))
 
-                TempEngineLoad = df['Engine Load(%)'].replace(to_replace="-", value="0")
+                TempEngineLoad = df['Engine Load(%)'].replace("-", value="0")
                 TempEngineLoad_np = np.asarray(TempEngineLoad)
                 TempEngineLoad_np = TempEngineLoad_np.astype(float)
 
                 fig, axs = plt.subplots(3)
                 axs[0].plot(CoolantTemperatureF_np, 'y')
-                axs[0].plot(SafeState1['Index'], CoolantTemperatureF_np[SafeState1['Index']], 'g.')
-                axs[0].plot(SafeState2['Index'], CoolantTemperatureF_np[SafeState2['Index']], 'r.')
-                axs[0].plot(SafeState3['Index'], CoolantTemperatureF_np[SafeState3['Index']], 'k.')
+                axs[0].plot(SafeState1['Index'],
+                            CoolantTemperatureF_np[SafeState1['Index']], 'g.')
+                axs[0].plot(SafeState2['Index'],
+                            CoolantTemperatureF_np[SafeState2['Index']], 'r.')
+                axs[0].plot(SafeState3['Index'],
+                            CoolantTemperatureF_np[SafeState3['Index']], 'k.')
 
                 axs[1].plot(State0, 'y')
                 axs[1].plot(SafeState1['Index'], SafeState1['Safestate'], 'g.')
                 axs[1].plot(SafeState2['Index'], SafeState2['Safestate'], 'r.')
                 axs[1].plot(SafeState3['Index'], SafeState3['Safestate'], 'k.')
-                # axs[1].title.set_text("Coolant Temperature in F")
 
                 axs[2].plot(TempEngineLoad_np, 'y')
-                axs[2].plot(np.repeat(EngineLoadThreshold, len(TempEngineLoad_np)))
-                axs[2].plot(SafeState1['Index'], TempEngineLoad_np[SafeState1['Index']], 'g.')
+                axs[2].plot(np.repeat(EngineLoadThreshold,
+                                      len(TempEngineLoad_np)))
+                axs[2].plot(SafeState1['Index'],
+                            TempEngineLoad_np[SafeState1['Index']], 'g.')
 
                 axs[1].axvline(x=IndexTripTimeThreshold)
                 axs[0].axvline(x=IndexTripTimeThreshold)
