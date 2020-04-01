daecacefb231a1dd73f6c831867c94b9b398dca4
prithvisekhar <prithvisekhar@gmail.com>
2020-04-01 10:42:36 +0530

    Update Run_Pipline.py

--- Function/Run_Pipline.py
+++ Function/Run_Pipline.py
@@ -7,7 +7,7 @@ for i in df_Pipeline.index:
 	f.write("cd "+str(df_Pipeline["FileName"][i])+"\n")
 	f.write("python Test*.py\n")
 	f.write("coverage run --source=.  Test*.py\n")
-	f.write("coverage report \n")
+	#f.write("coverage report \n")
 	f.write("coverage html \n")
 	
 	f.write("cd ..\n")
