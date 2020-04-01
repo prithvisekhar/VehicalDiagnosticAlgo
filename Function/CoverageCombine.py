3f7f6d59474fd550f9e023204207b0c28fba3e4b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-01 14:15:14 +0000

    Deploy prithvisekhar/VehicalDiagnosticAlgo to github.com/prithvisekhar/VehicalDiagnosticAlgo.git:gh-pages

--- Function/CoverageCombine.py
+++ Function/CoverageCombine.py
@@ -10,8 +10,7 @@ for x in FileNames:
 	if os.path.isfile(x+"/.coverage"):
 		FilePaths= FilePaths+ x +"/.coverage  "
 		f.write("git add -f "+x +"/.coverage  \n")
-
-
+	
 f.write("coverage combine "+FilePaths+ "\n")
 print("coverage combine "+FilePaths+ "\n")
 f.write("codeclimate-test-reporter  --token $CC_TEST_REPORTER_ID --file ./.coverage  \n")
--- Function/Coverage_Function.sh
+++ Function/Coverage_Function.sh
@@ -1,2 +1,4 @@
+git add -f DIAFuelMixture_FuelMixture/.coverage  
+git add -f DIASpeedVoilation_SpeedVoilation/.coverage  
 coverage combine  DIAFuelMixture_FuelMixture/.coverage  DIASpeedVoilation_SpeedVoilation/.coverage  
 codeclimate-test-reporter  --token $CC_TEST_REPORTER_ID --file ./.coverage  
