d1b6aa04f80852fd827e51253ca2be3e1d2feb75
prithvisekhar <prithvisekhar@gmail.com>
2020-04-01 10:53:39 +0530

    Update CoverageCombine.py

--- Function/CoverageCombine.py
+++ Function/CoverageCombine.py
@@ -9,6 +9,7 @@ for x in FileNames:
 	if os.path.isfile(x+"/.coverage"):
 		FilePaths= FilePaths+ x +"/.coverage  "
 	
-f.write("coverage combine "+FilePaths+ "\n")	
+f.write("coverage combine "+FilePaths+ "\n")
+print("coverage combine "+FilePaths+ "\n")
 f.write("codeclimate-test-reporter  --token $CC_TEST_REPORTER_ID --file ./.coverage  \n")
 f.close()
