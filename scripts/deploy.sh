3a4cb53461b534dd032ebe955909ff03bedebbea
prithvisekhar <prithvisekhar@gmail.com>
2020-04-01 12:52:50 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-git remote set-url origin git@github.com:prithvisekhar/VehicalDiagnosticAlgo/edit/master
+git remote set-url origin git@github.com:prithvisekhar/VehicalDiagnosticAlgo
 git add -p
 git commit -m "Updating Execution"
 git push -u origin master
