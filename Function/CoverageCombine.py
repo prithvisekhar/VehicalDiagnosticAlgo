6aedf139f19e48a5a24308e196ee78607a8d009d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-01 11:00:46 +0000

    Deploy prithvisekhar/VehicalDiagnosticAlgo to github.com/prithvisekhar/VehicalDiagnosticAlgo.git:gh-pages

--- Function/CoverageCombine.py
+++ Function/CoverageCombine.py
@@ -8,13 +8,9 @@ FilePaths=' '
 for x in FileNames:
 	if os.path.isfile(x+"/.coverage"):
 		FilePaths= FilePaths+ x +"/.coverage  "
-		
-
-if len(FileNames)>1:
-	f.write("coverage combine "+FilePaths+ "\n")
-	#print("coverage combine "+FilePaths+ "\n")
-else:
-	f.write("cp  "+FilePaths+ " .coverage \n")
+		#hf.write("git add -f "+x +"/.coverage  \n")
 	
+f.write("coverage combine "+FilePaths+ "\n")
+print("coverage combine "+FilePaths+ "\n")
 f.write("codeclimate-test-reporter  --token $CC_TEST_REPORTER_ID --file ./.coverage  \n")
 f.close()
