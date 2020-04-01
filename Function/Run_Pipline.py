4aec9e7a8253e66a8bd904dbbf1de3a959f8ee5a
prithvisekhar <prithvisekhar@gmail.com>
2020-04-01 10:24:37 +0530

    Add files via upload

--- Function/Run_Pipline.py
+++ Function/Run_Pipline.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 df_Pipeline = pd.read_csv("Running_Pipline.csv")
@@ -5,8 +6,10 @@ f = open("Running_Function.sh", "w")
 for i in df_Pipeline.index:
 	f.write("cd "+str(df_Pipeline["FileName"][i])+"\n")
 	f.write("python Test*.py\n")
+	f.write("coverage run --source=.  Test*.py\n")
+	f.write("coverage report \n")
+	f.write("coverage html \n")
+	
 	f.write("cd ..\n")
+
 f.close()
-
-
-
