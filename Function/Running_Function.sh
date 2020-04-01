1b42a51439832766a06ef11b3fb12acc6431253b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-01 05:10:55 +0000

    Deploy prithvisekhar/VehicalDiagnosticAlgo to github.com/prithvisekhar/VehicalDiagnosticAlgo.git:gh-pages

--- Function/Running_Function.sh
+++ Function/Running_Function.sh
@@ -4,3 +4,9 @@ coverage run --source=.  Test*.py
 coverage report 
 coverage html 
 cd ..
+cd DIASpeedVoilation_SpeedVoilation
+python Test*.py
+coverage run --source=.  Test*.py
+coverage report 
+coverage html 
+cd ..
