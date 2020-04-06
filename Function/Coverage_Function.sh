217f88d2c0dcce00e74a76aaf2b7e82e72dda9c7
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-04-06 11:04:42 +0530

    Delete Coverage_Function.sh

--- Function/Coverage_Function.sh
+++
@@ -1,2 +0,0 @@
-coverage combine .coverage  DIASpeedVoilation_SpeedVoilation/.coverage  DIAFuelMixture_FuelMixture/.coverage  
-codeclimate-test-reporter  --token $CC_TEST_REPORTER_ID --file ./.coverage  
