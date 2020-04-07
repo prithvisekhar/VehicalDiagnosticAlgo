9975b98b80473c504e9bb036051d37a9bf49251d
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-04-07 14:55:07 +0530

    Update Run_Pipline.py

--- Function/Run_Pipline.py
+++ Function/Run_Pipline.py
@@ -4,9 +4,9 @@ import pandas as pd
 df_Pipeline = pd.read_csv("Running_Pipline.csv")
 f = open("Running_Function.sh", "w")
 for i in df_Pipeline.index:
-	f.write("cd "+str(df_Pipeline["FileName"][i])+"\n")
-	t1=str(df_Pipeline["FileName"][i]).split('/')
 	
+	t1=str(df_Pipeline["FileName"][i]).split('/')
+	f.write("cd "+str(t1[0])+"\n")
 	f.write("python Test*.py\n")
 	if len(t1)>1:
 		f.write("coverage run  --source=. "+str(t1[1])+"/Test*.py\n")
@@ -17,7 +17,6 @@ for i in df_Pipeline.index:
 	f.write("coverage report \n")
 	f.write("coverage html \n")
 	
-	for k in range(0,len(t1)):
-		f.write("cd ..\n")
+	f.write("cd ..\n")
 
 f.close()
