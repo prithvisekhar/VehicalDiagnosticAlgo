fe3caf063796f464a67f933efd9eafb65ae9b6a7
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-04-07 16:45:02 +0530

    Update Run_Pipline.py

--- Function/Run_Pipline.py
+++ Function/Run_Pipline.py
@@ -6,18 +6,18 @@ f = open("Running_Function.sh", "w")
 for i in df_Pipeline.index:
 	
 	t1=str(df_Pipeline["FileName"][i]).split('/')
-	f.write("cd "+str(t1[0])+"\n")
-	f.write("python "+str(t1[1])+"/Test*.py\n")
+	f.write("cd "+str(df_Pipeline["FileName"][i])+"\n")
+	f.write("python Test*.py\n")
 	f.write("pwd\n")
 	if len(t1)>1:
-		f.write("coverage run   --source=.  "+" "+str(t1[1])+"/Test*.py\n")
-		print("coverage run   "+str(df_Pipeline["FileName"][i])+"/Test*.py\n")
+		f.write("coverage run   --source=.,../"+" /Test*.py\n")
+		print("coverage run   --source=.,../"+" /Test*.py\n"")
 	#else:
 		#f.write("coverage run --source="+str(t1[0])+" "+str(df_Pipeline["FileName"][i])+"/Test*.py\n")
 			
 	f.write("coverage report \n")
 	f.write("coverage html \n")
-	
-	f.write("cd ..\n")
+	for k in range(0,len(t1)):
+		f.write("cd ..\n")
 
 f.close()
