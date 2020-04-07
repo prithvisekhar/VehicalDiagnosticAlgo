19205e62e2acb9a6b7040f631518e82c9b5dfa39
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-04-07 15:54:05 +0530

    Update Run_Pipline.py

--- Function/Run_Pipline.py
+++ Function/Run_Pipline.py
@@ -6,12 +6,12 @@ f = open("Running_Function.sh", "w")
 for i in df_Pipeline.index:
 	
 	t1=str(df_Pipeline["FileName"][i]).split('/')
-	f.write("cd "+str(t1[0])+"\n")
+	#f.write("cd "+str(t1[0])+"\n")
 	f.write("python Test*.py\n")
 	f.write("pwd\n")
 	if len(t1)>1:
-		f.write("coverage run  --source=.,"+str(t1[1])+"/Test*.py"+" "+str(t1[1])+"/Test*.py\n")
-		print("coverage run  --source=.,"+str(t1[1])+"/Test*.py"+" "+str(t1[1])+"/Test*.py\n")
+		f.write("coverage run  --source="+str(t1[0])+" "+str(df_Pipeline["FileName"][i])+"/Test*.py\n")
+		print("coverage run  --source="+str(t1[0])+" "+str(df_Pipeline["FileName"][i])+"/Test*.py\n")
 	else:
 		f.write("coverage run --source=. Test*.py\n")
 			
