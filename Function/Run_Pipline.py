17ee224158bce7626fbd0905bc0b5a617003f02c
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-04-07 14:32:48 +0530

    Update Run_Pipline.py

--- Function/Run_Pipline.py
+++ Function/Run_Pipline.py
@@ -8,8 +8,12 @@ for i in df_Pipeline.index:
 	t1=str(df_Pipeline["FileName"][i]).split('/')
 	
 	f.write("python Test*.py\n")
-	f.write("coverage run --source="+str(t1[0])+" Test*.py\n")
-	print("coverage run --source= "+str(t1[0])+" Test*.py\n")
+	if len(t1)>1:
+		f.write("coverage run --source=.,"+str(t1[0])+" Test*.py\n")
+		print("coverage run --source="+str(df_Pipeline["FileName"][i])+","+str(t1[0])+" Test*.py\n")
+	else:
+		f.write("coverage run --source=. Test*.py\n")
+			
 	f.write("coverage report \n")
 	f.write("coverage html \n")
 	
