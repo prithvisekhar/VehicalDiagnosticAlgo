62b84aeb149f0a310bfba657867235fdc3f7f50f
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-04-07 17:08:36 +0530

    Update CoverageCombine.py

--- Function/CoverageCombine.py
+++ Function/CoverageCombine.py
@@ -10,8 +10,8 @@ for i in df_Pipeline.index:
 	FilePaths= FilePaths+str(df_Pipeline["FileName"][i]) +"/.coverage  "
 		
 	
-f.write("coverage combine  "+FilePaths+ "\n")
-print("coverage combine  "+FilePaths+ "\n")
+f.write("coverage combine .coverage  "+FilePaths+ "\n")
+print("coverage combine .coverage  "+FilePaths+ "\n")
 f.write("codeclimate-test-reporter  --token $CC_TEST_REPORTER_ID --file ./.coverage  \n")
 
 f.close()
