ae1bae9fa1dbf5dfdee6f1d94acc57de18e45cb1
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-04-08 18:02:41 +0530

    Update CoverageCombine.py

--- Function/CoverageCombine.py
+++ Function/CoverageCombine.py
@@ -4,14 +4,17 @@ import pandas as pd
 df_Pipeline = pd.read_csv("Running_Pipline.csv")
 
 f = open("Coverage_Function.sh", "w")
-
+flage=0
 FilePaths=' '
 for i in df_Pipeline.index:
-	FilePaths= FilePaths+str(df_Pipeline["FileName"][i]) +"/.coverage  "
+	if os.path.isfile(str(df_Pipeline["FileName"][i]) +"/.coverage"):
+		FilePaths= FilePaths+str(df_Pipeline["FileName"][i]) +"/.coverage  "
+		flage=1
 		
-	
-f.write("coverage combine .coverage  "+FilePaths+ "\n")
-print("coverage combine .coverage  "+FilePaths+ "\n")
+		
+if flage==1:	
+	f.write("coverage combine .coverage  "+FilePaths+ "\n")
+
 f.write("codeclimate-test-reporter  --token $CC_TEST_REPORTER_ID --file ./.coverage  \n")
 
 f.close()
