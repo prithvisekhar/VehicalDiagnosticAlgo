81e715bdb3b40bad223f935c5f555cea20daf91e
ganeshb15 <61306528+ganeshb15@users.noreply.github.com>
2020-04-08 18:11:10 +0530

    Update CoverageCombine.py

--- Function/CoverageCombine.py
+++ Function/CoverageCombine.py
@@ -7,7 +7,7 @@ f = open("Coverage_Function.sh", "w")
 flage=0
 FilePaths=' '
 for i in df_Pipeline.index:
-	if os.path.isfile(str(df_Pipeline["FileName"][i]) +"/.coverage"):
+	if os.path.exists(str(df_Pipeline["FileName"][i]) +"/.coverage"):
 		FilePaths= FilePaths+str(df_Pipeline["FileName"][i]) +"/.coverage  "
 		print('dsfdsfssfsf')
 		flage=1
