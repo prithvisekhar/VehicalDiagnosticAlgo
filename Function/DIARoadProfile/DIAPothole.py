f9c091efada34ba7036ff9d7b01dafcc94231113
Ashwiniks560 <52238308+Ashwiniks560@users.noreply.github.com>
2020-04-16 12:29:15 +0530

    Add files via upload

---
+++ Function/DIARoadProfile/DIAPothole.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[57]:
+
+
+from __future__ import division
+import gmplot
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import peakutils 
+import scipy
+
+def Pothole(Accelerometer_values):
+    Cutoff_freq = 0.2  
+    bandwidth = 0.16  
+    N = int(np.ceil((4 / bandwidth)))
+    if not N % 2: N += 1  # Make sure that N is odd.
+    n = np.arange(N)
+ 
+    #Compute a low-pass filter.
+    h = np.sinc(2 * Cutoff_freq * (n - (N - 1) / 2))
+    w = np.blackman(N)
+    h = h * w
+    h = h / np.sum(h)
+ 
+    #Create a high-pass filter from the low-pass filter through spectral inversion.
+    h = -h
+    h[(N - 1) // 2] += 1
+    signal = np.convolve(Accelerometer_values, h,'same')
+    np.set_printoptions(suppress=False)
+    signal1=signal.astype(float) #amplifying the output by 10 times for further computation
+    
+    values = np.array(signal1)
+    confirmed_potholes = values[values<-0.19]
+    indexvalues_confirmed = np.nonzero(signal1 <-0.19)
+    rest = np.array(signal1[signal1>-0.2])
+    possible_potholes = rest[rest<-0.1]
+    index_acc =  np.nonzero(signal1)
+    return confirmed_potholes,possible_potholes,indexvalues_confirmed,signal1, index_acc
+
---
+++ Function/DIARoadProfile/Test_function.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import DIAPothole
+import unittest
+
+
+class Test_Pothole(unittest.TestCase):
+    def testPothole(self):
+        try:
+            df_File = pd.read_csv("List_of_Data_Set.csv")
+            if not (os.path.isdir("Result")):
+                os.mkdir("Result")
+            for i in df_File.index:
+                df = pd.read_csv(str(df_File["Input_File_Name"][i]))
+                TempFile1 = df_File["Input_File_Name"][i].split('/')
+                TempFile3 = TempFile1[-1].split('.')
+                TempFile4 = str(TempFile3[0])
+                path = os.path.join("Result/", TempFile4)
+                time = df['Trip Time(Since journey start)(s)']
+                Accx = df['Acceleration Sensor(X axis)(g)'] 
+                Accy = df['Acceleration Sensor(Y axis)(g)']
+                Accz = df['Acceleration Sensor(Z axis)(g)']
+                Lat = df[' Latitude']
+                time_np=np.array(time)
+                confirmed_potholes,possible_potholes,indexvalues_confirmed,Acc_signal,Acc_index=DIAPothole.Pothole(Accy)
+                confirmed = np.array(indexvalues_confirmed)
+                idx=  np.nonzero(confirmed)
+                plt.figure(1)
+                plt.plot(time_np,Acc_signal,label='Vertical Acceleration')
+                plt.plot(time_np[idx[0][0]+confirmed[0]],confirmed_potholes,'r.',markersize=10,label='Detected Potholes')
+                #plt.plot(np.repeat(-0.1,len(d)),label='-0.1')
+                #plt.plot(np.repeat(-0.2,len(d)),label='-0.2')
+
+                plt.axhline(y=-0.1, color='m', linestyle='-',label='-0.1 Possibility Threshold of Pothole ')
+                plt.axhline(y=-0.2, color='y', linestyle='-',label='-0.2 Confirmation Threshold of Pothole ')
+                plt.title("Pothole Detection with Filtered Acceleration Data.")
+                plt.ylabel('Acceleration After High Pass Filter')
+                plt.xlabel('Time in Seconds')
+
+                plt.legend(loc='upper right')
+                plt.savefig(path+'.png')
+                print(confirmed_potholes)
+                print(indexvalues_confirmed[0])
+                plt.show()
+                
+        except AssertionError as e:
+            f = open("Error_Pothole", "a")
+            f.write("TestCase_no_0:\n\t" + str(e) + " \n")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
