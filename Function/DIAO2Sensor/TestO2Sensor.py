9c3d368fd60de6a3949d8c7cb2b1819376942998
sonalik23 <62057255+sonalik23@users.noreply.github.com>
2020-04-16 13:08:23 +0530

    Update TestO2Sensor.py

--- Function/DIAO2Sensor/TestO2Sensor.py
+++ Function/DIAO2Sensor/TestO2Sensor.py
@@ -1,7 +1,7 @@
 import os
 import unittest
-import pandas as pd
 import DIAO2Sensor
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
@@ -17,21 +17,25 @@ class Test_O2Sensor(unittest.TestCase):
                 TempFile3 = TempFile1[-1].split('.')
                 TempFile4 = str(TempFile3[0])
                 path = os.path.join("Result/", TempFile4)
-                mean_value = DIAO2Sensor.O2_Sensor\(df['02 Volts Bank 1 sensor 1(V)'])
-                plt.figure()
-                plt.plot(df['Trip Time(Since journey start)(s)'][0:100], df['O2 Volts Bank 1 sensor 1(V)'][0:100], marker='o', markerfacecolor='red')
-                plt.show()
-                plt.title("Trip Time vs O2 Sensor")
-                plt.xlabel("Time")
-                plt.ylabel("O2 in volts")
+                mean_value = DIAO2Sensor.O2_Sensor(df['02 Volts Bank 1 sensor 1(V)'])
+                plt.figure(figsize=(10, 6))
+                plt.plot(df['Trip Time(Since journey start)(s)'][:100], df['O2 Volts Bank 1 sensor 1(V)'][:100],
+                         marker='o', markerfacecolor='red')
+                plt.axhline(y=0.45, color='r', linestyle='-', label='Ideal Condition')
+                plt.axhline(y=0.8, color='g', linestyle='--', label='Rich Condition')
+                plt.axhline(y=0.2, color='brown', linestyle='--', label='Lean Condition')
+                plt.legend(loc='best')
+                plt.xlabel("Trip Time in secs")
+                plt.ylabel("O2 Sensor in volts")
+                plt.savefig(path + '.png')
                 plt.close()
-        
+
         except:
             e = sys.exc_info()
             print("In Test Failed")
             f = open("Error_O2Sensor", "w")
             f.write("TestCase_no_0:\n\t" + str(e) + " \n")
 
-
 if __name__ == '__main__':
     unittest.main()
+
