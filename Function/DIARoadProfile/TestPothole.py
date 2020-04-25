51dc2ddf690f4e9ba425d984dad2e0118ef003f2
Ashwiniks560 <52238308+Ashwiniks560@users.noreply.github.com>
2020-04-25 21:58:39 +0530

    Update TestPothole.py

--- Function/DIARoadProfile/TestPothole.py
+++ Function/DIARoadProfile/TestPothole.py
@@ -18,15 +18,15 @@ class Test_Pothole(unittest.TestCase):
             df_File = pd.read_csv("List_of_Data_Set.csv")
             if not (os.path.isdir("Result1")):
                 os.mkdir("Result1")
-            if not (os.path.isdir("Result2")):
-                os.mkdir("Result2")
+            #if not (os.path.isdir("Result2")):
+                #os.mkdir("Result2")
             for i in df_File.index:
                 df = pd.read_csv(str(df_File["Input_File_Name"][i]))
                 TempFile1 = df_File["Input_File_Name"][i].split('/')
                 TempFile3 = TempFile1[-1].split('.')
                 TempFile4 = str(TempFile3[0])
                 path = os.path.join("Result1/", TempFile4)
-                path1 = os.path.join("Result2/", TempFile4)
+                #path1 = os.path.join("Result2/", TempFile4)
                 time = df['Trip Time(Since journey start)(s)']
                 Accx = df['Acceleration Sensor(X axis)(g)'] 
                 Accy = df['Acceleration Sensor(Y axis)(g)']
@@ -50,17 +50,17 @@ class Test_Pothole(unittest.TestCase):
                 plt.ylabel('Acceleration After High Pass Filter')
                 plt.xlabel('Time in Seconds')
                 plt.legend(loc='upper right')
-                
-                figure(2)
-                plt.plot(time,Speed, label='Speed')
-                plt.plot(time,Acc_pedal, label='Accelerator pedal position')
-                plt.plot(time,RPM, label='RPM')
-                plt.ylabel('Parameters')
-                plt.xlabel('Time in Seconds')
-                plt.legend(loc='upper right')
+                plt.savefig(path+'.png')
+                #figure(2)
+                #plt.plot(time,Speed, label='Speed')
+                #plt.plot(time,Acc_pedal, label='Accelerator pedal position')
+                #plt.plot(time,RPM, label='RPM')
+                #plt.ylabel('Parameters')
+                #plt.xlabel('Time in Seconds')
+                #plt.legend(loc='upper right')
 
-                plt.legend(loc='upper right')
-                plt.savefig(path1+'.png')
+                
+                #plt.savefig(path1+'.png')
                 
                 
                 
