1424a8f33861ebe1710c9ccca8a345ce506d5661
Shreyash-310 <81311058+Shreyash-310@users.noreply.github.com>
2021-08-03 19:19:01 +0530

    Final changes before merging

--- Function/CatalyticConvertor/catalytic_performace.py
+++ Function/CatalyticConvertor/catalytic_performace.py
@@ -1,10 +1,12 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+
 
 def check_performance(Upstream_O2, downstream_O2, Time, Speed):
     plt.subplot(2, 1, 1)
     plt.plot(Time, Upstream_O2)
     plt.plot(Time, downstream_O2)
+    plt.axhline(y=0.6, color='g', linestyle='--')
+    plt.axhline(y=0.9, color='g', linestyle='--')
     plt.xlabel('time')
     plt.ylabel('O2 sensor value')
     plt.legend(['Upstream', 'downstrem'], loc='lower right')
